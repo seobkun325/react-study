23238d6c0e1d312be02d186db21750d53fec9bf4
seobkun325 <seobkun325@gmail.com>
2025-04-24 19:47:41 +0900

    refactor : item 클래스로 변경

--- react-study/src/components/course/CourseListCard.jsx
+++ react-study/src/components/course/CourseListCard.jsx
@@ -1,7 +1,11 @@
 import CourseItem from "./CourseItem";
 export default function CourseListCard(props) {
+  // const invertedColor = {
+  //   background: "black",
+  //   color: "white",
+  // };
   return (
-    <div className="card">
+    <div className="card" style={{ background: "black", color: "white" }}>
       <div className="card__header">강의 목록</div>
       <div className="card__body">
         <div className="courses">
