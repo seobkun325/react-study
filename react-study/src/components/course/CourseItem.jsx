fa5b8f5a2370df80e6a536f5372963e9a2299873
seobkun325 <seobkun325@gmail.com>
2025-04-24 21:49:13 +0900

    feat : 컴포넌트 내에서 조건부 랜더링

--- react-study/src/components/course/CourseItem.jsx
+++ react-study/src/components/course/CourseItem.jsx
@@ -1,10 +1,35 @@
-export default function CourseItem({ title, description, thumbnail }) {
+function HeartIconBtn({ isFavorite = false }) {
+  return (
+    <button className="btn">
+      {/* {isFavorite ? (
+        <img className="icon-heart" src="/img/heart-fill-icon.svg" />
+      ) : (
+        <img className="icon-heart" src="/img/heart-icon.svg" />
+      )} */}
+      <img className="btn__img" src={isFavorite ? "/img/heart-fill-icon.svg" : "/img/heart-icon.svg"} />
+    </button>
+  );
+}
+
+function LinkIconBtn({ link }) {
+  return (
+    <a className="btn" href={link} target="__blank" rel="noreferrer">
+      <img className="btn__img" src="/img/link-icon.svg" alt="" />
+    </a>
+  );
+}
+
+export default function CourseItem({ title, description, thumbnail, isFavorite, link }) {
   return (
     <article className="course">
       <img className="course__img" src={thumbnail} alt="강의 이미지" />
       <div className="course__body">
         <div className="course__title">{title}</div>
         <div className="course__description">{description}</div>
+      </div>
+      <div className="course__icons">
+        <HeartIconBtn isFavorite={isFavorite} />
+        {link && <LinkIconBtn link={link} />}
       </div>
     </article>
   );
