5a01bf622b283f314a2bbd1ac0f9b0e8c73d6f68
seobkun325 <seobkun325@gmail.com>
2025-04-24 20:06:44 +0900

    refactor : props로 데이터 넘기기

--- react-study/src/components/course/CourseListCard.jsx
+++ react-study/src/components/course/CourseListCard.jsx
@@ -1,17 +1,19 @@
 import CourseItem from "./CourseItem";
-export default function CourseListCard(props) {
+export default function CourseListCard({ items }) {
   // const invertedColor = {
   //   background: "black",
   //   color: "white",
   // };
+  const [course1, course2, course3] = items;
   return (
-    <div className="card" style={{ background: "black", color: "white" }}>
+    // style={{ background: "black", color: "white" }}
+    <div className="card">
       <div className="card__header">강의 목록</div>
       <div className="card__body">
         <div className="courses">
-          <CourseItem />
-          <CourseItem />
-          <CourseItem />
+          <CourseItem {...course1} />
+          <CourseItem {...course2} />
+          <CourseItem {...course3} />
         </div>
       </div>
     </div>
