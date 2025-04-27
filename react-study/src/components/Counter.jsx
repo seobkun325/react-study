435b21b596f5ba9f0b85feecbe55e046a4ebeb43
seobkun325 <seobkun325@gmail.com>
2025-04-27 21:44:56 +0900

    feat : useState로 상태 기억

--- react-study/src/components/Counter.jsx
+++ react-study/src/components/Counter.jsx
@@ -1,5 +1,11 @@
-function Counter() {
-  return <button>Counter</button>;
-}
+import { useState } from "react";
 
-export default Counter;
+export default function Counter({ onTotal }) {
+  const [counter, setCounter] = useState(0);
+
+  const handleCounter = () => {
+    setCounter(counter + 1);
+    onTotal();
+  };
+  return <button onClick={handleCounter}>Counter : {counter}</button>;
+}
--- react-study/src/components/Main.jsx
+++ react-study/src/components/Main.jsx
@@ -1,9 +1,19 @@
 import Counter from "./Counter";
-
+import { useState } from "react";
 function Main() {
+  const [total, setTotal] = useState(0);
+  const handleTotal = () => {
+    setTotal(total + 1);
+  };
   return (
     <main>
-      <Counter />
+      {total}
+      <br />
+      <br />
+      <Counter onTotal={handleTotal} />
+      <br />
+      <br />
+      <Counter onTotal={handleTotal} />
     </main>
   );
 }
